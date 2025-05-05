24ba3daadc783bedd7eb661c1b9b3591d66e9176
RocketMenace <grizzlygit777@gmail.com>
2025-05-05 04:08:50 +0300

    build:
    Implementing PreferenceRepository layer.

---
+++ app/repository/preference.py
@@ -0,0 +1,13 @@
+from typing import Annotated, Any
+
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.models.user_models import Interest
+from app.repository.base_repository import BaseRepository
+
+
+class PreferenceRepository(BaseRepository):
+    def __init__(self, session: Annotated[AsyncSession, Depends()]):
+        self.session = session
+        super().__init__(session=session, model=Interest)
