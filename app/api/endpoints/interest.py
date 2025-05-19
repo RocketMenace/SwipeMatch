e75dd0c2e5e56e2110d9fd641aa51306c4c791bf
RocketMenace <grizzlygit777@gmail.com>
2025-05-19 21:56:21 +0300

    build:
    Implementing dependencies in routers.

--- app/api/endpoints/interest.py
+++ app/api/endpoints/interest.py
@@ -2,6 +2,7 @@ from typing import Annotated
 
 from fastapi import APIRouter, Depends, status
 
+from app.dependencies.service import get_interest_service
 from app.schemas.user_schemas import Interest, InterestIn
 from app.services.interest import InterestService
 
@@ -10,6 +11,6 @@ router = APIRouter(prefix="/interest", tags=["Interests"])
 
 @router.post(path="/add", status_code=status.HTTP_201_CREATED, response_model=Interest)
 async def add_interest(
-    data: InterestIn, service: Annotated[InterestService, Depends()]
+    data: InterestIn, service: Annotated[InterestService, Depends(get_interest_service)]
 ):
     return await service.add_interest(data)
--- app/api/endpoints/preference.py
+++ app/api/endpoints/preference.py
@@ -2,6 +2,7 @@ from typing import Annotated
 
 from fastapi import APIRouter, Depends, status
 
+from app.dependencies.service import get_preference_service
 from app.schemas.user_schemas import Preference, PreferenceIn
 from app.services.preference import PreferenceService
 
@@ -10,6 +11,7 @@ router = APIRouter(prefix="/preferences", tags=["Preferences"])
 
 @router.post("/add", status_code=status.HTTP_201_CREATED, response_model=Preference)
 async def add_preference(
-    data: PreferenceIn, service: Annotated[PreferenceService, Depends()]
+    data: PreferenceIn,
+    service: Annotated[PreferenceService, Depends(get_preference_service)],
 ):
     return await service.add_preference(data=data)
